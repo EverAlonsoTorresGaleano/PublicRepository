a2b7cd8d814962eb88ef8415dc42434aed785e19
Ever Alonso Torres Galeano <eeatg844@hotmail.com>
2019-07-15 12:00:09 -0500

    Include Script for Populate Data

---
+++ MasGlobal.HandsOn.DB/PopulateData/PopulateData.sql
@@ -0,0 +1,14 @@
+﻿
+INSERT INTO [dbo].[DocumentTypes] ([DocumentTypeName])VALUES('C.C.')
+INSERT INTO [dbo].[DocumentTypes] ([DocumentTypeName])VALUES('C.E.')
+
+INSERT INTO [dbo].[Employees]([DocumentTypeFk],[DocumentNumber],[Name],[LastName],[PaymentValue],[ContractTypeFk])VALUES(1,3481844,'Ever Alonso','Torres Galeano',5,1)
+INSERT INTO [dbo].[Employees]([DocumentTypeFk],[DocumentNumber],[Name],[LastName],[PaymentValue],[ContractTypeFk])VALUES(1,1111111,'Piter','Languila',200,2)
+INSERT INTO [dbo].[Employees]([DocumentTypeFk],[DocumentNumber],[Name],[LastName],[PaymentValue],[ContractTypeFk])VALUES(1,2222222,'Pibe','Valderrama',10,1)
+INSERT INTO [dbo].[Employees]([DocumentTypeFk],[DocumentNumber],[Name],[LastName],[PaymentValue],[ContractTypeFk])VALUES(1,3333333,'Nairo','Quintana',250,2)
+INSERT INTO [dbo].[Employees]([DocumentTypeFk],[DocumentNumber],[Name],[LastName],[PaymentValue],[ContractTypeFk])VALUES(1,4444444,'James','Rodriguez',80,1)
+INSERT INTO [dbo].[Employees]([DocumentTypeFk],[DocumentNumber],[Name],[LastName],[PaymentValue],[ContractTypeFk])VALUES(1,5555555,'Radamel','Falcao',210,2)
+INSERT INTO [dbo].[Employees]([DocumentTypeFk],[DocumentNumber],[Name],[LastName],[PaymentValue],[ContractTypeFk])VALUES(1,6666666,'Mariana','Pajon',20,1)
+INSERT INTO [dbo].[Employees]([DocumentTypeFk],[DocumentNumber],[Name],[LastName],[PaymentValue],[ContractTypeFk])VALUES(1,7777777,'Silvestre','Dangon',300,2)
+INSERT INTO [dbo].[Employees]([DocumentTypeFk],[DocumentNumber],[Name],[LastName],[PaymentValue],[ContractTypeFk])VALUES(1,8888888,'Rigoberto','Uran',30,1)
+INSERT INTO [dbo].[Employees]([DocumentTypeFk],[DocumentNumber],[Name],[LastName],[PaymentValue],[ContractTypeFk])VALUES(1,9999999,'Pedro','Perez',300,2)
